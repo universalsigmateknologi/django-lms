015ea2966d21285d6cbb586ff4e7edcd65c82b80
universalsigmateknologi <universalsigmateknologi@gmail.com>
2026-05-14 08:24:44 +0700

    feat: initialize project UI with Tailwind CSS integration and course management templates

--- config/settings.py
+++ config/settings.py
@@ -36,7 +36,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    "jazzmin",
+    # "jazzmin",
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
